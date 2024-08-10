8cc639cef7a5264f33e7a3359d5848e9b7a3b4f5
vhaguiar07 <vhaguiar7@gmail.com>
2024-08-09 23:12:52 -0300

    Colocando o Sidebar em todas as páginas do app

--- aspem-front/src/App.tsx
+++ aspem-front/src/App.tsx
@@ -2,15 +2,22 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './views/home/pages/home';
 import UserPage from './views/user/pages/user';
+import Sidebar from './components/sidebar/Sidebar'; // Ajuste o caminho conforme necessário
 import { USERS } from './views/user/routes';
+import './styles/global.css'; // Importa o CSS global
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path={USERS()} element={<UserPage />} />
-      </Routes>
+      <div className="app-container">
+        <Sidebar className="sidebar" />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path={USERS()} element={<UserPage />} />
+          </Routes>
+        </div>
+      </div>
     </Router>
   );
 };
