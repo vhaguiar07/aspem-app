80f166ca362ef1ca1b9e05e55c8929346736fd2e
vhaguiar07 <vhaguiar7@gmail.com>
2024-08-22 13:29:55 -0300

    styling autarquias list page

--- aspem-front/src/views/autarquia/pages/Autarquia.tsx
+++ aspem-front/src/views/autarquia/pages/Autarquia.tsx
@@ -7,7 +7,6 @@ import './autarquiaStyles.css';
 const AutarquiaPage: React.FC = () => {
   const [autarquias, setAutarquias] = useState<Autarquia[]>([]);
   const [filteredAutarquias, setFilteredAutarquias] = useState<Autarquia[]>([]);
-  const [searchTerm, setSearchTerm] = useState('');
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
@@ -25,36 +24,24 @@ const AutarquiaPage: React.FC = () => {
     loadAutarquias();
   }, []);
 
-  useEffect(() => {
-    setFilteredAutarquias(
-      autarquias.filter(autarquia =>
-        autarquia.nomeSocio.toLowerCase().includes(searchTerm.toLowerCase())
-      )
-    );
-  }, [searchTerm, autarquias]);
-
   return (
     <div className="autarquia-page">
       <div className="search-container">
-        <div className="search-bar-container">
-          <input
-            type="text"
-            placeholder="Pesquisar autarquias"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            className="search-bar"
-          />
-          <Icon icon="search" className="search-icon" />
+        <div className="nice-form-group">
+          <input type="search" placeholder="Nome/CPF" value="" />
         </div>
       </div>
-      <h1 className='list-title'>Lista de Autarquias</h1>
+      <div className="div-title">
+        <h1>Autarquias</h1>
+      </div>
       {error && <p>{error}</p>}
       <div className="table-container">
-        <table>
+        <table className="table">
           <thead>
             <tr>
               <th>Nome Socio</th>
               <th>CPF</th>
+              <th>Ações</th>
             </tr>
           </thead>
           <tbody>
@@ -62,6 +49,9 @@ const AutarquiaPage: React.FC = () => {
               <tr key={autarquia.id}>
                 <td>{autarquia.nomeSocio}</td>
                 <td>{autarquia.cpf}</td>
+                <td>
+                  <Icon icon="edit" className="edit-icon" onClick={() => handleEdit(autarquia.id)} />
+                </td>
               </tr>
             ))}
           </tbody>
@@ -69,6 +59,11 @@ const AutarquiaPage: React.FC = () => {
       </div>
     </div>
   );
+
+  function handleEdit(id: string) {
+    // Aqui você pode adicionar a lógica para a ação de edição
+    console.log(`Editando autarquia com ID: ${id}`);
+  }
 };
 
 export default AutarquiaPage;
