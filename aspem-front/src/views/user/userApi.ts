784b4af2c78280660d15bcfc44419f3055458269
vhaguiar07 <vhaguiar7@gmail.com>
2024-08-24 14:35:01 -0300

    implementing jwt auth at user list page

--- aspem-front/src/views/user/userApi.ts
+++ aspem-front/src/views/user/userApi.ts
@@ -5,7 +5,22 @@ const api = axios.create({
   baseURL: 'http://localhost:8080',
 });
 
+api.interceptors.request.use(config => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers['Authorization'] = `Bearer ${token}`;
+  }
+  return config;
+}, error => {
+  return Promise.reject(error);
+});
+
 export const fetchUsers = async (url: string): Promise<User[]> => {
-  const response = await api.get(url);
-  return response.data;
+  try {
+    const response = await api.get(url);
+    return response.data;
+  } catch (error) {
+    console.error('Erro ao buscar usuários:', error);
+    throw error;
+  }
 };
